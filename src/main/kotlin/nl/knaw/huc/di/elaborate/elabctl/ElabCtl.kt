71414f0ab0ab1978a904fe4fb61791fba67be68a
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2024-06-11 17:13:38 +0200

    make zipfile of facsimiles and tei

--- src/main/kotlin/nl/knaw/huc/di/elaborate/elabctl/ElabCtl.kt
+++ src/main/kotlin/nl/knaw/huc/di/elaborate/elabctl/ElabCtl.kt
@@ -1,7 +1,12 @@
 package nl.knaw.huc.di.elaborate.elabctl
 
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.zip.ZipEntry
 import java.util.zip.ZipFile
+import java.util.zip.ZipOutputStream
 import kotlin.io.path.Path
 import kotlin.io.path.writeText
 import kotlin.system.measureTimeMillis
@@ -40,10 +45,11 @@ fun main(args: Array<String>) {
 @OptIn(ExperimentalSerializationApi::class)
 fun archive(args: List<String>) {
     logger.info { "args=${args}" }
-    File("build/zip").deleteRecursively()
-    File("build/zip/facsimiles").mkdirs()
     if (args.size > 1) {
         val warPath = args[1]
+        val projectName = warPath.split('/').last().replace(".war", "")
+        File("build/zip/$projectName").deleteRecursively()
+        File("build/zip/$projectName/facsimiles").mkdirs()
         logger.info { "<= $warPath" }
         ZipFile(warPath).use { zip ->
             val elabConfigEntry = zip.getEntry("data/config.json")
@@ -57,22 +63,65 @@ fun archive(args: List<String>) {
                 logger.info { entryDescription }
                 val teiName = teiName(entryTypeName, i + 1, entryDescription.shortName)
                 val entry = loadEntry(zip, entryDescription)
-                storeFacsimiles(teiName, entry.facsimiles)
+                storeFacsimiles(projectName, teiName, entry.facsimiles)
 //                logger.info { entry.metadata }
                 val tei = entry.toTEI(teiName)
-                val teiPath = "build/zip/${teiName}.xml"
+                val teiPath = "build/zip/$projectName/${teiName}.xml"
                 logger.info { "=> $teiPath" }
                 Path(teiPath).writeText(tei)
             }
         }
+        createZip(projectName)
     }
 }
 
-fun storeFacsimiles(baseName: String, facsimiles: ArrayList<Facsimile>) {
+private fun createZip(projectName: String) {
+    val sourceFile = "build/zip/$projectName/"
+    val zipPath = "$projectName-archive.zip"
+    logger.info("=> $zipPath")
+    FileOutputStream(zipPath).use { fos ->
+        ZipOutputStream(fos).use { zipOut ->
+            val fileToZip = File(sourceFile)
+            zipFile(fileToZip, fileToZip.name, zipOut)
+        }
+    }
+}
+
+@Throws(IOException::class)
+private fun zipFile(fileToZip: File, fileName: String, zipOut: ZipOutputStream) {
+    if (fileToZip.isHidden) {
+        return
+    }
+    if (fileToZip.isDirectory) {
+        if (fileName.endsWith("/")) {
+            zipOut.putNextEntry(ZipEntry(fileName))
+            zipOut.closeEntry()
+        } else {
+            zipOut.putNextEntry(ZipEntry("$fileName/"))
+            zipOut.closeEntry()
+        }
+        val children: Array<File> = fileToZip.listFiles() ?: emptyArray()
+        for (childFile in children) {
+            zipFile(childFile, "$fileName/${childFile.name}", zipOut)
+        }
+        return
+    }
+    FileInputStream(fileToZip).use { fis ->
+        val zipEntry = ZipEntry(fileName)
+        zipOut.putNextEntry(zipEntry)
+        val bytes = ByteArray(1024)
+        var length: Int
+        while ((fis.read(bytes).also { length = it }) >= 0) {
+            zipOut.write(bytes, 0, length)
+        }
+    }
+}
+
+fun storeFacsimiles(projectName: String, baseName: String, facsimiles: ArrayList<Facsimile>) {
     facsimiles.forEachIndexed { i, f ->
         val url = f.thumbnail.replace("/adore-djatoka.*localhost:8080".toRegex(), "")
         logger.info { url }
-        val filePath = "build/zip/facsimiles/${baseName}-${(i + 1).toString().padStart(2, '0')}.jp2"
+        val filePath = "build/zip/$projectName/facsimiles/${baseName}-${(i + 1).toString().padStart(2, '0')}.jp2"
         runBlocking {
             val bytes: ByteArray = HttpClient(CIO).get(url).body<ByteArray>()
             logger.info { "=> $filePath" }
