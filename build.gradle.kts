934a2e405f81c4e4cdb479053b40130228a42a9c
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-04-10 13:12:16 +0200

    sort dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,15 @@ buildscript {
 val ktorVersion: String by project
 dependencies {
     implementation(kotlin("reflect"))
+
+//    implementation("app.cash.barber:barber:2024.01.12.173005-933c241")
     implementation("ch.qos.logback:logback-classic:1.5.6")
+    implementation("com.github.spullara.mustache.java:compiler:0.9.10")
     implementation("com.google.code.gson:gson:2.10")
+    implementation("de.digitalcollections.iiif:iiif-apis:0.3.11") // iiif v2
+    implementation("info.freelibrary:jiiify-presentation-v3:0.12.4") // iiif v3
+    implementation("io.arrow-kt:arrow-core:2.0.1")
+    implementation("io.arrow-kt:arrow-fx-coroutines:2.0.1")
     implementation("io.ktor:ktor-client-cio:${ktorVersion}")
     implementation("io.ktor:ktor-client-core:${ktorVersion}")
     implementation("nl.knaw.huygens:visitei:0.6.2")
@@ -43,12 +50,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0") // 1.7.0 leads to compile error
     implementation("org.jsoup:jsoup:1.17.2")
     implementation("org.redundent:kotlin-xml-builder:1.9.1")
-//    implementation("app.cash.barber:barber:2024.01.12.173005-933c241")
-    implementation("com.github.spullara.mustache.java:compiler:0.9.10")
-    implementation("info.freelibrary:jiiify-presentation-v3:0.12.4") // iiif v3
-    implementation("de.digitalcollections.iiif:iiif-apis:0.3.11") // iiif v2
-    implementation("io.arrow-kt:arrow-core:2.0.1")
-    implementation("io.arrow-kt:arrow-fx-coroutines:2.0.1")
 
     runtimeOnly("com.github.jai-imageio:jai-imageio-jpeg2000:1.4.0") // jpeg2000 handling in imageio
 
