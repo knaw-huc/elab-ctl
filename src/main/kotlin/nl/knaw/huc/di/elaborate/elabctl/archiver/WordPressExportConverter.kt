fd60aca1635061a5ab460c71e110edde88cbf300
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-08-14 12:19:15 +0200

    refactoring; build `about` xml

--- src/main/kotlin/nl/knaw/huc/di/elaborate/elabctl/archiver/WordPressExportConverter.kt
+++ src/main/kotlin/nl/knaw/huc/di/elaborate/elabctl/archiver/WordPressExportConverter.kt
@@ -8,6 +8,8 @@ import javax.xml.xpath.XPathFactory
 import kotlin.io.path.Path
 import kotlin.io.path.writeText
 import org.apache.logging.log4j.kotlin.logger
+import org.redundent.kotlin.xml.PrintOptions
+import org.redundent.kotlin.xml.xml
 import org.w3c.dom.Node
 import org.w3c.dom.NodeList
 
@@ -53,43 +55,102 @@ class WordPressExportConverter(private val outputDir: String) {
             .newDocumentBuilder()
         logger.info { "<= $xmlPath" }
         builder.parse(xmlPath).let { doc ->
-            val nodes = doc.getNodeList("//item")
-            for (i in 0..<nodes.length) {
-                val itemNode = nodes.item(i)
-                val link = itemNode.getNode("./link")?.textContent
-                val title = itemNode.getNode("./title")?.textContent
-                val content = itemNode.getNode("./content:encoded")?.textContent
-                if (content?.isNotBlank() ?: false) {
-                    logger.info { "item $i:\n  title=$title\n  link=$link\n  content='$content'" }
+            doc.getNodeSequence("//item")
+                .filter { node -> node.hasContent() }
+                .filter { node -> !node.postName().startsWith("hello-world") }
+                .filter { node -> node.status() == "publish" }
+                .forEach { itemNode ->
+                    val content = itemNode.getString("./content:encoded/text()")
+                    val link = itemNode.getString("./link/text()")
+                    val creator = itemNode.getString("./dc:creator/text()")
+                    val title = itemNode.getString("./title/text()")
+                    val postName = itemNode.postName()
+                    val lastModified = itemNode.getString("./wp:post_modified/text()")
+//                    logNode(title, lastModified, link, postName, creator, content)
+                    val xmlSrc = buildXML(content, link, creator, title, lastModified)
+                    val outPath = "$outputDir/$postName.xml"
+                    logger.info { "=> $outPath" }
+                    Path(outPath).writeText(xmlSrc)
                 }
-            }
-            logger.info { "${nodes.length} item nodes found" }
         }
-
-        val outPath = "$outputDir/about.xml"
-        logger.info { "=> $outPath" }
-        Path(outPath).writeText("<xml/>")
 
         return errors
     }
+
+    val printOptions = PrintOptions(
+        singleLineTextElements = true,
+        indent = "  ",
+        useSelfClosingTags = true
+    )
+
+    private fun buildXML(content: String, link: String, creator: String, title: String, lastModified: String): String =
+        xml("TEI") {
+            "text" {
+                comment("link = $link")
+                comment("creator = $creator")
+                comment("title = $title")
+                comment("lastModified = $lastModified")
+                "body" {
+                    unsafeText(content)
+                }
+            }
+        }.toString(printOptions = printOptions)
+
+    private fun logNode(
+        title: String,
+        lastModified: String,
+        link: String,
+        postName: String,
+        creator: String,
+        content: String
+    ) {
+        logger.info {
+            """item:
+      title=$title
+      modified=$lastModified
+      link=$link
+      post-name='$postName'
+      creator='$creator'
+      content='$content'"""
+        }
+    }
+
+    private fun Node.postName(): String =
+        getString("./wp:post_name/text()")
+
+    private fun Node.status(): String =
+        getString("./wp:status/text()")
+
+    private fun Node.hasContent(): Boolean =
+        getString("./content:encoded/text()").isNotBlank()
+
+    private fun NodeList.asSequence(): Sequence<Node> =
+        sequence {
+            for (i in 0..<this@asSequence.length) {
+                yield(item(i))
+            }
+        }
+
+    private fun Node.getNodeSequence(xpathExpression: String): Sequence<Node> =
+        this.getNodeList(xpathExpression).asSequence()
 
     private fun Node.getNodeList(xpathExpression: String): NodeList =
         xpath.compile(xpathExpression)
             .evaluate(this, XPathConstants.NODESET) as NodeList
 
-    private fun Node.getNode(xpathExpression: String): Node? =
+    private fun Node.getNode(xpathExpression: String): Node =
         xpath.compile(xpathExpression)
-            .evaluate(this, XPathConstants.NODE) as? Node
+            .evaluate(this, XPathConstants.NODE) as Node
 
-    private fun Node.getNumber(xpathExpression: String): Number? =
+    private fun Node.getNumber(xpathExpression: String): Number =
         xpath.compile(xpathExpression)
-            .evaluate(this, XPathConstants.NUMBER) as? Number
+            .evaluate(this, XPathConstants.NUMBER) as Number
 
-    private fun Node.getString(xpathExpression: String): String? =
+    private fun Node.getString(xpathExpression: String): String =
         xpath.compile(xpathExpression)
-            .evaluate(this, XPathConstants.STRING) as? String
+            .evaluate(this, XPathConstants.STRING) as String
 
-    private fun Node.getBoolean(xpathExpression: String): Boolean? =
+    private fun Node.getBoolean(xpathExpression: String): Boolean =
         xpath.compile(xpathExpression)
-            .evaluate(this, XPathConstants.BOOLEAN) as? Boolean
+            .evaluate(this, XPathConstants.BOOLEAN) as Boolean
 }
