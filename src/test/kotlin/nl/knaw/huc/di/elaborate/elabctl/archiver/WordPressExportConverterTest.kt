145178d79a4870cc2fe0fa320fd6d9f82f27cc7b
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-08-14 12:17:43 +0200

    test both wp exports

--- src/test/kotlin/nl/knaw/huc/di/elaborate/elabctl/archiver/WordPressExportConverterTest.kt
+++ src/test/kotlin/nl/knaw/huc/di/elaborate/elabctl/archiver/WordPressExportConverterTest.kt
@@ -5,10 +5,18 @@ import org.junit.jupiter.api.Test
 class WordPressExportConverterTest {
 
     @Test
-    fun `test WordPressExportConverter`() {
-        val projectName = "correspondentie-bolland-en-cosijn"
+    fun `test WordPressExportConverter with correspondentie-bolland-en-cosijn`() {
+        testWPExportForProject("correspondentie-bolland-en-cosijn")
+    }
+
+    @Test
+    fun `test WordPressExportConverter with brieven-correspondenten-1900`() {
+        testWPExportForProject("brieven-correspondenten-1900")
+    }
+
+    private fun testWPExportForProject(projectName: String) {
         val wpePath = "data/${projectName.replace("elab4-", "")}-wpe.xml"
-        val outputDir = "/tmp"
+        val outputDir = "out"
         val errors = WordPressExportConverter(outputDir).convert(wpePath)
         assert(errors.isEmpty())
     }
