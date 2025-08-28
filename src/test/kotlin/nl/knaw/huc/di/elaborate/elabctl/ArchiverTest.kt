c2563498b1b42e88dee06b138b1bf3363ad818be
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-08-28 10:58:40 +0200

    convert &nbsp; to horizontal <space>

---
+++ src/test/kotlin/nl/knaw/huc/di/elaborate/elabctl/ArchiverTest.kt
@@ -0,0 +1,27 @@
+package nl.knaw.huc.di.elaborate.elabctl
+
+import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test
+import nl.knaw.huc.di.elaborate.elabctl.archiver.Archiver
+import nl.knaw.huc.di.elaborate.elabctl.archiver.TEIBuilder.convertHorizontalSpace
+import nl.knaw.huc.di.elaborate.elabctl.archiver.TEIBuilder.horizontalSpaceTag
+
+class ArchiverTest {
+
+    //    @Test
+    fun `test bolland-cosijn`() {
+        Archiver.archive(listOf("./data/elab4-correspondentie-bolland-en-cosijn.war"))
+    }
+
+    @Test
+    fun `converting nbsp to horizontal space`() {
+        val oneNbsp = "one<nbsp/>space"
+        val oneSpace = "one" + horizontalSpaceTag(1) + "space"
+        assertEquals(oneSpace, oneNbsp.convertHorizontalSpace())
+
+        val twoNbsp = "two<nbsp/><nbsp/>spaces"
+        val twoSpace = "two" + horizontalSpaceTag(2) + "spaces"
+        assertEquals(twoSpace, twoNbsp.convertHorizontalSpace())
+    }
+
+}
